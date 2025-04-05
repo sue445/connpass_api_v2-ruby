96fde7f7262fa059829bf2e98cfead6f07c6f58a
sue445 <sue445@sue445.net>
2025-04-05 17:01:49 +0900

    Impl get_user_groups

--- lib/connpass_api_v2/client/user_methods.rb
+++ lib/connpass_api_v2/client/user_methods.rb
@@ -21,6 +21,24 @@ module ConnpassApiV2
 
         connection.get("users/", params.compact).body # steep:ignore NoMethod
       end
+
+      # Get user groups
+      #
+      # @param nickname [String]
+      # @param start [Integer,nil]
+      # @param count [Integer,nil]
+      #
+      # @return [ConnpassApiV2::Response]
+      #
+      # @see https://connpass.com/about/api/v2/#tag/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC/operation/connpass_account_account_api_v2_views_user_group
+      def get_user_groups(nickname, start: nil, count: nil)
+        params = {
+          start: start,
+          count: count,
+        }
+
+        connection.get("users/#{nickname}/groups/", params.compact).body # steep:ignore NoMethod
+      end
     end
   end
 end
